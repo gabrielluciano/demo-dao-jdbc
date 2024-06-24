88d96897674635d09122aebebe015bfa4db1fc2f
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-06-24 10:49:11 -0300

    implements SellerDaoJDBC findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -61,6 +64,47 @@ public class SellerDaoJDBC implements SellerDao {
             }
 
             return seller;
+        } catch (SQLException e) {
+            throw new DbException("Error getting Seller: " + e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(stmt);
+        }
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            stmt = conn.prepareStatement("""
+                    SELECT seller.*, department.Name as DepName
+                    FROM seller INNER JOIN department
+                    ON seller.DepartmentId = department.Id
+                    WHERE department.Id = ?
+                    ORDER BY Name
+                        """);
+            stmt.setInt(1, department.getId());
+            rs = stmt.executeQuery();
+
+            List<Seller> sellers = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(dep.getId(), dep);
+                }
+
+                Seller seller = instantiateSeller(rs, dep);
+                sellers.add(seller);
+            }
+
+            return sellers;
         } catch (SQLException e) {
             throw new DbException("Error getting Seller: " + e.getMessage());
         } finally {
